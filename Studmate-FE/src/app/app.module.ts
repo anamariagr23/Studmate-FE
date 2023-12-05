17785776dbf8818d5b307d9110f1ec2e4756c581
Anamaria Groza <anamariagroza81@gmail.com>
2023-12-05 21:31:54 +0200

    #1 created login component and routing for it

--- Studmate-FE/src/app/app.module.ts
+++ Studmate-FE/src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LoginComponent} from './login/login.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // TO DO - future improvement use routing module for that
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
