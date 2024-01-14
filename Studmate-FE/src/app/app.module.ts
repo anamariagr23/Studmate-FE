04e95736d1a8aaeaa1d97061a07d09cd086382c2
Anamaria Groza <anamariagroza81@gmail.com>
2024-01-14 17:29:05 +0200

    added avatar component

--- Studmate-FE/src/app/app.module.ts
+++ Studmate-FE/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {LoginComponent} from './login/login.component';
-import {RouterModule} from "@angular/router";
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {UsersPageComponent} from './users-page/users-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersPageComponent } from './users-page/users-page.component';
 import { SurveyPageComponent } from './survey-page/survey-page.component';
 import { SurveyFormComponent } from './survey-form/survey-form.component';
+import { AvatarComponent } from './avatar-page/avatar/avatar.component';
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import { SurveyFormComponent } from './survey-form/survey-form.component';
     UsersPageComponent,
     SurveyPageComponent,
     SurveyFormComponent,
+    AvatarComponent,
 
   ],
   imports: [
@@ -28,14 +30,15 @@ import { SurveyFormComponent } from './survey-form/survey-form.component';
     BrowserAnimationsModule,
     // TO DO - future improvement use routing module for that
     RouterModule.forRoot([
-      {path: 'login', component: LoginComponent},
-      {path: 'users', component: UsersPageComponent},
-      {path: 'survey', component: SurveyPageComponent},
+      { path: 'login', component: LoginComponent },
+      { path: 'users', component: UsersPageComponent },
+      { path: 'survey', component: SurveyPageComponent },
     ]),
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
